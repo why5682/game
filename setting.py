43f58ad1777490f8f272a081088f479e332071e2
why5682 <why5682@gmail.com>
2024-01-23 01:44:07 +0900

    사냥 시스템 도입

--- setting.py
+++ setting.py
@@ -7,9 +7,12 @@ for i in range(1,200):
     exp_table[i+1]=int(exp_table[i]+((1.05+2*(1/i))*(exp_table[i]-exp_table[i-1]))) #i:n 의 의미 i레벨 에서 다음레벨로 가려면 n만큼의 exp가 필요하다
 
 monster_book={
-    '달팽이':(1,30,10,1,5,10,0,5, False),
-    '파란달팽이':(3,50,20,1,8,12,0.01,7,False),
-    '빨간달팽이':(5,120,100,1.5,10,18,0.02,30,False)
+    '달팽이':(1,30,5,1,5,5,0,5, False),
+    '파란달팽이':(3,50,10,1,8,7,0.01,7,False),
+    '빨간달팽이':(5,70,30,1.5,10,18,0.02,30,False),
+    '테스트':(1,1,1,1,1,0,1,100000000,False),
+    '아이즈':(31,750,300,1.2,70,40,0.05,550,False),
+    '발록':(110,100000,1000,0.2,100,70,0.1,1000,True)
     }
 class character:
     def __init__(self,name):
@@ -22,10 +25,10 @@ class character:
         self.level=1
         self.att=self.str*1+self.dex*0.2
         self.mana=self.int*1
-        self.speed=1    #10이 최대, 먼저 10채우면 행동 가능
+        self.speed=1.1    #10이 최대, 먼저 10채우면 행동 가능
         self.acc=self.dex*1+self.str*0.2  #명중요구치와 비교
         self.avo=self.luk*1    #회피율함수 필요, 몬스터의 명중치를 구현해야함
-        self.defe=1-(1/(self.str*1))   #퍼센트로 데미지 감소(방어율), 마딜은 고정딜로 할 생각
+        self.defe=1-(100/(self.str)+90)   #퍼센트로 데미지 감소(방어율), 마딜은 고정딜로 할 생각
         self.crit=self.dex*0.5
         self.crid=self.luk*1
         self.pei=0  #관통, 방어력 무시, 0~1의 값
@@ -42,7 +45,7 @@ class character:
         self.mana=self.int*1
         self.acc=self.dex*1+self.str*0.2  #명중요구치와 비교
         self.avo=self.luk*1    #회피율함수 필요, 몬스터의 명중치를 구현해야함
-        self.defe=1-(1/(self.str*1))   #퍼센트로 데미지 감소(방어율), 마딜은 고정딜로 할 생각
+        self.defe=1-(100/(self.str+90))   #퍼센트로 데미지 감소(방어율), 마딜은 고정딜로 할 생각
         self.crit=self.dex*0.5
         self.crid=self.luk*1
         self.hp=int(math.log(self.level)*(30+self.level*(5+0.5*self.str+0.25*self.dex+0.125*(self.int+self.luk))))+50
@@ -63,6 +66,10 @@ class character:
                 self.update_status()
                 break
 
+    def full(self):
+        self.hp_now=self.hp
+        self.mp_now=self.mp
+
     def str_up(self,how):
         self.ap-=how
         self.str+=how
@@ -82,9 +89,6 @@ class character:
         self.ap-=how
         self.luk+=how
         print(f'행운을 {how}만큼 올려 {self.luk}이 되었습니다.\n')
-
-    def hit_by_mon(self,damage):
-        self.hp_now-=damage
 
     def show_exp(self):
         print('='*10)
@@ -112,7 +116,7 @@ class character:
         print(f'공격속도 : {self.speed}')
         print(f'명중치 : {round(self.acc,2)}')
         print(f'회피치 : {round(self.avo,2)}')
-        print(f'방어율 : {round(self.defe,2)}%')
+        print(f'방어율 : {round(self.defe,2)*100}%')
         print(f'크리티컬 확률 : {self.crit}%')
         print(f'크리티컬 데미지 : {self.crid}%')
         print('-'*10)
@@ -137,7 +141,7 @@ def hit(character,monster):
     return hit
 
 def damage(character,monster):  #몬스터 방어율 기본적으로 0~0.5가 맞을듯, 보스 부터 1이상도 가능하게 하기
-    damage=character.att-random.randint(0,int(character.att/2))
+    damage=character.att-random.randint(0,int(character.att*(0.6+0.2*(1/(character.acc-5)))))
     damage=damage*(1-(monster.mon_defe*(1-character.pei)))
     if damage <= 0:
         damage = 1
@@ -152,7 +156,7 @@ def avoid(character,monster):
     return avo
 
 def injury(character, monster):
-    mond=monster.mon_att-random.randint(0,int(monster.mon_att/4))
+    mond=monster.mon_att-random.randint(0,int(monster.mon_att/3))
     if monster.mon_skill == True:
         return round(mond), True
     else:
@@ -163,8 +167,6 @@ def judge_turn(character, monster,a=0, b=0 ):
     while a < 10 and b < 10:
         a+=character.speed
         b+=monster.mon_speed
-        print(a, b)
-
         if a >= 10 and b >= 10 :
             a-=10
             b-=10
@@ -202,7 +204,7 @@ def pro_turn(turn,character,monster):
             damag,cri=damage(character,monster)
             monster.mon_hp-=damag
             if cri == True:
-                print(f'{monster.mon_name}{damag}!의 피해를 주었습니다.')
+                print(f'{monster.mon_name}에게{damag}!의 피해를 주었습니다.')
             else:
                 print(f'{monster.mon_name}에게{damag}의 피해를 주었습니다.')
         else:
@@ -235,11 +237,13 @@ def end_turn(character,monster):
         
 def turn_go(character,monster):
     a,b=0,0
-    while end_turn(character,monster) == False:
+    while end_turn(character,monster) == False: #이게 문제인듯함
         re=judge_turn(character,monster,a,b)
         a,b=re[1],re[2]
         pro_turn(re[0],character,monster)
-        input()
+    character.show_status()
+    cho=input('어떤 행동을 하시겠습니까?\n1.마을\n2.포션 사용\n3.ap 사용\n또는 계속하려면 아무 키나 누르세요.\n')
+    return cho
     
 def stat_up(character):
     character.judge_level()
@@ -247,20 +251,15 @@ def stat_up(character):
     if character.ap == 0:
         print('남은 ap포인트가 없습니다.\n')
         return
-
     while True:
         ans = input('무엇을 올립니까?\n1.힘\n2.민첩\n3.지력\n4.행운\n5.취소\n : ')
-        
         try:
             ans = int(ans)
-
             if ans in [1, 2, 3, 4, 5]:
                 if ans == 5:
                     return
-
                 while True:
                     how = input('얼마나 올립니까?\n')
-
                     try:
                         how = int(how)
                         if how <= character.ap:
@@ -272,35 +271,64 @@ def stat_up(character):
                                 character.int_up(how)
                             elif ans == 4:
                                 character.luk_up(how)
-
                             character.update_status()
                             return  # Exit the function after successful stat increase
-
                         print('잘못된 입력입니다.')
                     except ValueError:
                         print('잘못된 입력입니다.')
-
-                print('잘못된 입력입니다.')
             else:
                 print('잘못된 입력입니다.')
         except ValueError:
             print('잘못된 입력입니다.')
 
+def battle(character,monste,num):
+    for i in range(num):
+        encount=monster(monste)
+        a=turn_go(character,encount)
+        if a == '1':
+            print('마을로 돌아갑니다.')
+            character.full()
+            return False
+        elif a == '2':
+            print('포션을 사용합니다.')
+            character.hp_now+=100
+            if character.hp_now > character.hp:
+                character.hp_now = character.hp
+        elif a == '3':
+            print('ap를 사용합니다.')
+            stat_up(character)
+    print('설정된 전투가 모두 끝났습니다.\n')
+
+def set_battle(character):
+    while True:
+        ans = input('어떤 몬스터를 사냥하시겠습니까?\n1.취소\n')
+        if ans == '1':
+            print('사냥을 취소하고 마을로 돌아갑니다.')
+            break
+        try:
+            monster_book[ans]
+            while True:
+                how = input('몇 번 사냥하시겠습니까?\n')
+                try:
+                    how = int(how)
+                    if how > 0:
+                        battle(character, ans, how)
+                        a=input('더 할래? 안할거면 1') #임시 조치
+                        return a
+                    else:
+                        print('사냥 횟수는 양수여야 합니다. 다시 입력하세요.')
+                except ValueError:
+                    print('잘못된 입력입니다.')
+                break  # Exit the second while loop once a valid input is received
+        except KeyError:
+            print('해당 몬스터가 존재하지 않습니다. 다시 입력하세요.')
+
+
+
 playe=character('eheh')
-
-for i in range(30):
-    encount=monster('달팽이')
-    playe.show_status()
-    turn_go(playe,encount)
-    stat_up(playe)
-    playe.show_status()
-
-for i in range(30):
-    encount=monster('파란달팽이')
-    playe.show_status()
-    turn_go(playe,encount)
-    stat_up(playe)
-    playe.show_status()
+a=0
+while a != '1':
+    a=set_battle(playe)
 #class skill:
 
 #class item:
